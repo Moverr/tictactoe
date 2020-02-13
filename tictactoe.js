254d3a80cc1a7c611d89e3557c0f73e3759634ec
Muyinda Rogers <moverr@gmail.com>
2020-02-13 15:14:20 +0300

    :Debugging :

--- tictactoe.js
+++ tictactoe.js
@@ -1,18 +1,21 @@
+ 
 
+ function  initBoard () {
 
-
-const initBoard = () => {
     let board = [];
     for (let i = 0; i < 3; i++) {
         let boradrow = []
         board[i] = [];
         for (let j = 0; j < 3; j++) {
-            row[j] = j;
+            boradrow[j] = j;
         }
-        board[i] = row;
+        board[i] = boradrow;
     }
+    
+
     return board;
 }
 
+// let result  = initBoard();
 
 console.log(initBoard());
